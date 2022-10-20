9de0a11fdae5122843094a7cabab7eac16b8a99a
shreyas0924 <shreyasp0924@gmail.com>
2022-10-20 22:39:37 +0530

    updated nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 export default function Nav() {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="flex justify-between items-center py-10">
+            <Link href='/'>
+                <button className="text-lg font-medium">Creative Minds</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link href='/auth/login'>
+                    <a className="py-2 px-4 text-sm bg-cyan-600 font-medium ml-8 text-white rounded-lg">Join Now</a>
+                </Link>
+            </ul>
+        </nav>
 
     )
 }
